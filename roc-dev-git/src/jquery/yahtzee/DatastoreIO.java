2795554d8b6a4d205b8a67d815ca4812ae61b910
Piet de Vries <p.h.devries@gmail.com>
2015-08-15 00:18:06 +0200

    Yahtzee - veranderen hoogste scores
    Alleen hoogste score van een speler vertonen in scorebord

--- roc-dev-git/src/jquery/yahtzee/DatastoreIO.java
+++ roc-dev-git/src/jquery/yahtzee/DatastoreIO.java
@@ -6,22 +6,46 @@ import java.util.Date;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 
 public class DatastoreIO {
 	DatastoreService datastore;
 	
+	public static final int ONGEWIJZIGD = 0;
+	public static final int GEWIJZIGD = 1;
+	public static final int TOEGEVOEGD = 2;
+	
 	public DatastoreIO() {
 		datastore = DatastoreServiceFactory.getDatastoreService();
 	}
 	
-	public void voegScoreToe(Yahtzee yahtzee) {
-		Entity ent = new Entity("YahtzeeScores", yahtzee.getGebruikersnaam() + yahtzee.getDatumTijd());
-		ent.setProperty("datum", yahtzee.getDatumTijd());
-		ent.setProperty("naam", yahtzee.getGebruikersnaam());
-		ent.setProperty("score", yahtzee.getScore());
-		datastore.put(ent);
+	public int voegScoreToe(Yahtzee yahtzee) {
+		int ret = ONGEWIJZIGD;
+		Key k = KeyFactory.createKey("YahtzeeScores", yahtzee.getGebruikersnaam());
+		Entity e = new Entity("YahtzeeScores", yahtzee.getGebruikersnaam());
+		try {
+			e = datastore.get(k);
+			int hoogsteScore = (int) (long) e.getProperty("score");
+			int nieuweScore = yahtzee.getScore();
+			if (nieuweScore > hoogsteScore) {
+				hoogsteScore = nieuweScore;
+				e.setProperty("datum", yahtzee.getDatumTijd());
+				e.setProperty("score", hoogsteScore);
+				datastore.put(e);
+				ret = GEWIJZIGD;
+			}
+		} catch (EntityNotFoundException e1) {
+			e.setProperty("datum", yahtzee.getDatumTijd());
+			e.setProperty("naam", yahtzee.getGebruikersnaam());
+			e.setProperty("score", yahtzee.getScore());
+			datastore.put(e);
+			ret = TOEGEVOEGD;
+		}
+		return ret;
 	}
 	
 	public ArrayList<Yahtzee> getAlleScores() {
@@ -37,5 +61,4 @@ public class DatastoreIO {
 		}
 		return scores;
 	}
-
 }
